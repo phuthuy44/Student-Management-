d9194318afa1e56e4fdc1adc2e6898779a117d34
Phuong vy <vy002359@gmail.com>
2022-09-11 00:03:28 +0700

    Done UI dashboard

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ for beginners(Core PHP)
 ![image](https://user-images.githubusercontent.com/88236906/189470245-6888e4b3-14f3-475d-815b-7964deaf2504.png)
 2. Form Login:
 ![image](https://user-images.githubusercontent.com/88236906/189470221-1adde78e-e695-4314-a15d-3b234ad81749.png)
+3. Form Dashboard:
+![image](https://user-images.githubusercontent.com/88236906/189493971-4df327f2-d7e9-4056-9bfb-c8b94bcecf49.png)
--- admin/dashboard.php
+++ admin/dashboard.php
@@ -19,14 +19,59 @@ include_once "header.php";
                 <p class="text-center bg-danger p-3">Hello,Admin!</p>
             </div>
             <ul class="listgroup list-group-flush">
-                <a href="" class="list-group-item list-group-item-action"><i class="fa fa-dashboard"></i>Dashboard</a>
-                <a href="" class="list-group-item list-group-item-action"><i class="fa fa-user"></i>Add Student</a>
-                <a href="" class="list-group-item list-group-item-action"><i class="fa fa-eye"></i>View Student</a>
-                <a href="" class="list-group-item list-group-item-action"><i class="fa fa-pencil"></i>Edit Student</a>
-                <a href="" class="list-group-item list-group-item-action"><i class="fa fa-sign-out"></i>Logout</a>
+                <a href="../../Student-Management-/admin/dashboard.php" class="list-group-item list-group-item-action"><i class="fa fa-dashboard"></i>Dashboard</a>
+                <a href="../../Student-Management-/admin/addstudent.php" class="list-group-item list-group-item-action"><i class="fa fa-user"></i>Add Student</a>
+                <a href="../../Student-Management-/admin/viewstudent.php" class="list-group-item list-group-item-action"><i class="fa fa-eye"></i>View Student</a>
+                <a href="../../Student-Management-/admin/updatestudent.php" class="list-group-item list-group-item-action"><i class="fa fa-pencil"></i>Edit Student</a>
+                <a href="../../Student-Management-/page/logout.php" class="list-group-item list-group-item-action"><i class="fa fa-sign-out"></i>Logout</a>
 
             </ul>
         </div>
+        <div class="main-body">
+        <section class="main-form">
+        <h2 class="text-center text-white .shadow-lg bg-danger p-3 font-weight-bold">Student Managerment System</h2>
+        <div class="container bg-danger rounded dash mt-5" id="formsetting">
+            <h3 class="text-center text-white pb-4 pt-2 font-weight-bold">Welcome to dashboard</h3>
+            <div class="row">
+                <div class="col-md-4 col-sm-4 col-12 m-auto icon">
+                    <a href="../../Student-Management-/admin/addstudent.php" class="text-white text-center"><i class="fa fa-user"></i>
+                    <h3>Add student</h3>
+                </a>
+                </div>
+                <div class="col-md-4 col-sm-4 col-12 m-auto icon">
+                    <a href="../../Student-Management-/admin/viewstudent" class="text-white text-center"><i class="fa fa-eye"></i>
+                    <h3>View student</h3>
+                </a>
+                </div>
+                <div class="col-md-4 col-sm-4 col-12 m-auto icon">
+                    <a href="../../Student-Management-/admin/updatestudent.php" class="text-white text-center"><i class="fa fa-pencil"></i>
+                    <h3>Edit student</h3>
+                </a>
+                </div>
+            </div>
+            <hr class="text-white">
+
+            <div class="row">
+                <div class="col-md-4 col-sm-4 col-12 m-auto icon">
+                    <a href="" class="text-white text-center"><i class="fa fa-user"></i>
+                    <h3>Add teacher</h3>
+                </a>
+                </div>
+                <div class="col-md-4 col-sm-4 col-12 m-auto icon">
+                    <a href="" class="text-white text-center"><i class="fa fa-eye"></i>
+                    <h3>View teacher</h3>
+                </a>
+                </div>
+                <div class="col-md-4 col-sm-4 col-12 m-auto icon">
+                    <a href="" class="text-white text-center"><i class="fa fa-pencil"></i>
+                    <h3>Edit teacher</h3>
+                </a>
+                </div>
+            </div>
+            <hr>
+        </div>
+    </section>
+    </div>
     </div>
 
 
