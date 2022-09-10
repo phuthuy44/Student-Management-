1081743c5ca13f35dc17c1c275f5114310730392
Phuong vy <vy002359@gmail.com>
2022-09-10 12:11:48 +0700

    Done UI login & register

--- index.php
+++ index.php
@@ -1,43 +1,69 @@
+<?php
+session_start();
+if(isset($_SESSION['id'])){
+    header("Location:../../../../Student-Management-/page/login.php");
+}
+?>
 <!Doctype html>
 <html>
     <head>
         <meta charset="UTF-8">
         <title>Student Management System</title>
-        <link rel="stylesheet" href="./css/css.css">
+        <link rel="stylesheet" href="./css/login.css">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+
+        <!-- jQuery library -->
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+
+        <!-- Popper JS -->
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+
     </head>
     <body>
-    <h5 align="right" style="margin-right:20px"><a href="../Student-Management-/page/login.php">Admin Login</a></h5>
-    <h1 align="center">Welcome to Student Managerment System</h1>
-    
-    <form action="index.php" method="post">
-    <table style="width:40%" align="center" border="1">
-        <tr>
-            <td colspan="2" align="center">Student Information</td>
-        </tr>
-        <tr>
-            <td align="left">Choose Standed</td>
-            <td>
-                <select name="std" required>
-                    <option value="1">1st</option>
-                    <option value="2">2nd</option>
-                    <option value="3">3rd</option>
-                    <option value="4">4th</option>
-                    <option value="5">5th</option>
-                    <option value="6">6th</option>
-                </select>
-            </td>
-        </tr>
-        <tr>
-            <td align="left">Enter Rollno</td>
-            <td>
-                <input type="text" name="rollno" required>
-            </td>
-        </tr>
-        <tr>
-            <td colspan="2" align="center"><input type="submit" name="submit" value="Show Inform"> </td>
-        </tr>
-    </table>
-    </form>
+    <section>
+        <h1 class="text-center text-danger pt-5 pb-4 font-weight-bold">Student Management System</h1>
+        <div class="container shadow-sm">
+            <h3 class="text-danger text-center p-3" >Admin Login | Register Panel</h3>
+            
+            <div class="row">
+                <div class="col-md-7 col-sm-7 col-12">
+                    <img src="././image/school_software_1.png" alt=""width=100% height=100%>
+                </div>
+                <div class="col-md-5 col-sm-5 col-12 mt-4">
+                    <form action="" method="post">
+                        <div class="form-group">
+                            <label for="" class="text-danger font-weight-bold">Full name</label>
+                            <input type="text" name="fullname" placeholder="Enter your full name" class="form-control border">
+                        </div>
+                        <div class="form-group">
+                            <label for="" class="text-danger font-weight-bold">Email</label>
+                            <input type="email" name="email" placeholder="Enter your email" class="form-control border">
+                        </div>
+                        <div class="form-group">
+                            <label for="" class="text-danger font-weight-bold">Password</label>
+                            <input type="password" name="password" placeholder="Enter your password" class="form-control border">
+                        </div>
+                        <div class="form-group">
+                            <label for="" class="text-danger font-weight-bold">Confirm Password</label>
+                            <input type="password" name="password2" placeholder="Enter your confirm password" class="form-control border">
+                        </div>
+                        <div class="d-flex align-items-center">
+                        <input type="submit" name="submit" value="Register" class="btn btn-dark px-4 m-2">
+                        <a href="../Student-Management-/page/login.php">
+                        <div class="btn btn-dark px-4 m-2">Login</div>
+                        </div>
 
+                        
+                    </form>
+                    
+                    
+                </div>
+            </div>
+        </div>
+    </section>
     </body>
 </html>
--- page/login.php
+++ page/login.php
@@ -9,30 +9,56 @@ if(isset($_SESSION['id'])){
     <head>
         <meta charset="UTD-8">
         <title>Admin Login</title>
+        <link rel="stylesheet" href="./../../Student-Management-/css/login.css">
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+
+        <!-- jQuery library -->
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
+
+        <!-- Popper JS -->
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
+
     </head>
     <body>
-        <h1 align="center">Admin Login</h1>
-        <form action="login.php" method="post">
-            <table align="center">
-                <tr>
-                    <td>Username</td>
-                    <td>
-                        <input type="text" name="username"required>
-                    </td>
-                </tr>
-                <tr>
-                    <td>Password</td>
-                    <td>
-                        <input type="password" name="password"required>
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="2" align="center">
-                    <input type="submit" name="login" value="Login">
-                    </td>
-                </tr>
-            </table>
-        </form>
+    <section>
+        <h1 class="text-center text-danger pt-5 pb-4 font-weight-bold">Student Management System</h1>
+        <div class="container shadow-sm">
+            <h3 class="text-danger text-center p-3" >Admin Login</h3>
+            
+            <div class="row">
+                <div class="col-md-7 col-sm-7 col-12 mt-4">
+                    <img src="../image/school_software_1.png" class="img-fluid">
+                </div>
+                <div class="col-md-5 col-sm-5 col-12">
+                    <form action="" method="post">
+                        
+                        <div class="form-group">
+                            <label for="" class="text-danger font-weight-bold">Email</label>
+                            <input type="email" name="email" placeholder="Enter your email" class="form-control border">
+                        </div>
+                        <div class="form-group">
+                            <label for="" class="text-danger font-weight-bold">Password</label>
+                            <input type="password" name="password" placeholder="Enter your password" class="form-control border">
+                        </div>
+                        <div class="d-flex align-items-center">
+                        <input type="submit" name="login" value="Login" class="btn btn-dark px-4 m-2">
+                        <a href="../index.php">
+                        <div class="btn btn-dark px-4 m-2">Register</div>
+                        </a>
+                        </div>
+
+                        
+                    </form>
+                    </div>
+                    
+                
+            </div>
+        </div>
+    </section>
     </body>
 </html>
 <?php
